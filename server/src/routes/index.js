d41f66568e6c00191664516d85c1633e9967d7a3
Akshay Patel <akskanupatel@gmail.com>
2020-01-11 17:07:29 +0530

    minor update

--- server/src/routes/index.js
+++
@@ -1,4 +0,0 @@
-const cryptoRouter = require("./crypto.router");
-const fiatRouter = require("./fiat.router");
-
-module.exports = { cryptoRouter, fiatRouter };
--- server/src/tokens/index.js
+++
@@ -1,4 +0,0 @@
-const idex = require("./idex");
-const uniswap = require("./uniswap");
-
-module.exports = [...idex, ...uniswap];
