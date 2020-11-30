4240a0132edb92bc6a205690f07f448cf3270cf0
Akshay Patel <akshay.ap95@gmail.com>
2020-11-30 18:27:28 +0530

    UI:Add success icon after successful transfer

--- src/components/fiat/TokenTransferSuccessful.tsx
+++ src/components/fiat/TokenTransferSuccessful.tsx
@@ -1,16 +1,22 @@
 import { Button, Link, Typography } from "@material-ui/core";
 import React from "react";
 import { usePayFiat } from "../../context/PayFiatContext";
+import CheckCircleIcon from "@material-ui/icons/CheckCircle";
 
 const TokenTransferSuccessful = () => {
   const { setModalOpen, transactionHash, env } = usePayFiat();
 
   return (
     <>
-      <Typography align="center">Tokens successfully transferred.</Typography>
-      <Typography variant="caption">
-        Transaction hash:{transactionHash}
+      <Typography align="center" variant="h5">
+        Tokens successfully transferred
       </Typography>
+      <CheckCircleIcon style={{ fill: "green" }} />
+      <Typography align="center" variant="h6">
+        Transaction hash
+      </Typography>
+      <Typography variant="caption">{transactionHash}</Typography>
+      <br />
       <Link
         target="_blank"
         href={
@@ -21,11 +27,12 @@ const TokenTransferSuccessful = () => {
       >
         Click here to view on etherscan
       </Link>
+
       <Typography align="center">
         Please refresh page to make another transaction.
       </Typography>
       <Button
-        fullWidth
+        style={{ width: "75%" }}
         onClick={() => setModalOpen(false)}
         variant="contained"
         color="primary"
